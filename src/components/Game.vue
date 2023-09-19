e5946cc30de72a74bb1c7fce4b930b0fd9057f58
kotet912 <kotet912@yandex.ru>
2023-09-19 13:30:21 +0300

    Процесс-бар и Звезды

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,15 +1,19 @@
 <template>
     <div class="game">
         <div class="game__line">
-            <div class="game__line__process" :style="{ width: gameProgress + '%' }">
-            </div>
+            <div class="game__line__process" :style="{ width: gameProgress + '%' }"></div>
         </div>
 
         <div class="game__box">
             <span>0</span>
             <div class="game__box__threshold-points" v-for="stage in stages" :key="stage.id">
-                <span v-for="game in stage.games" :key="game.name">
-                    {{ game.bestResult }} / {{ stage.thresholdPoints }}
+                <img :src="totalPoints >= stage.thresholdPoints ? require('@/assets/Star.svg') : require('@/assets/StarContr.svg')"
+                    alt="">
+                <span v-if="totalPoints < stage.thresholdPoints">
+                    {{ totalPoints }} / {{ stage.thresholdPoints }}
+                </span>
+                <span v-else>
+                    {{ stage.thresholdPoints }}
                 </span>
             </div>
         </div>
@@ -20,21 +24,45 @@
 export default {
     name: 'DivTestGame',
     computed: {
-        gameProgress () {
-            const totalPoints = this.$store.state.stages.reduce((sum, stage) => {
-                return sum + stage.games.reduce((stageSum, game) => stageSum + game.bestResult, 0);
-            }, 0);
-
-            const totalThresholdPoints = this.$store.state.stages.reduce((sum, stage) => {
-                return sum + stage.thresholdPoints;
-            }, 0);
-
-            const progress = (totalPoints / totalThresholdPoints) * 100;
-
-            return Math.min(progress, 100);
-        },
         stages () {
             return this.$store.state.stages;
+        },
+        totalPoints () {
+            return this.$store.state.stages.reduce((sum, stage) => {
+                return sum + stage.games.reduce((stageSum, game) => stageSum + game.bestResult, 0);
+            }, 0);
+        },
+        gameProgress () {
+            const stageCount = this.stages.length;
+            if (stageCount === 0) {
+                return 0; // Избегаем деления на ноль
+            }
+
+            // Рассчитываем ширину каждого этапа в процентах
+            const stageWidth = 100 / stageCount;
+
+            // Найдем индекс текущего этапа, который не пройден
+            let currentIndex = -1;
+            for (let i = 0; i < stageCount; i++) {
+                if (this.totalPoints < this.stages[i].thresholdPoints) {
+                    currentIndex = i;
+                    break;
+                }
+            }
+
+            if (currentIndex === -1) {
+                return 100; // Если все этапы пройдены, процент равен 100%
+            }
+
+            if (currentIndex === 0) {
+                // Если текущий этап первый, используем общее количество очков
+                return (this.totalPoints / this.stages[currentIndex].thresholdPoints) * stageWidth;
+            } else {
+                // Рассчитываем текущую ширину процесс-бара на основе индекса текущего этапа
+                const currentWidth = (currentIndex * stageWidth) + ((this.totalPoints - this.stages[currentIndex - 1].thresholdPoints) / (this.stages[currentIndex].thresholdPoints - this.stages[currentIndex - 1].thresholdPoints)) * stageWidth;
+
+                return Math.min(currentWidth, 100);
+            }
         },
     },
 };
--- src/store.js
+++ src/store.js
@@ -10,7 +10,7 @@ export default createStore({
                 "games": [
                     {
                         "name": "Игра 1.1",
-                        "bestResult": 10,
+                        "bestResult": 14,
                         "isPlayed": false
                     }
                 ]
@@ -22,11 +22,64 @@ export default createStore({
                 "games": [
                     {
                         "name": "Игра 2.1",
-                        "bestResult": 30,
+                        "bestResult": 15,
                         "isPlayed": false
                     }
                 ]
             },
+            {
+                "id": 3,
+                "name": "Этап третий",
+                "thresholdPoints": 100,
+                "games": [
+                    {
+                        "name": "Игра 3.1",
+                        "bestResult": 100,
+                        "isPlayed": false
+                    }
+                ]
+            },
+            {
+                "id": 4,
+                "name": "Этап четвертый",
+                "thresholdPoints": 200,
+                "games": [
+                    {
+                        "name": "Игра 4.1",
+                        "bestResult": 0,
+                        "isPlayed": false
+                    }
+                ]
+            },
+            {
+                "id": 5,
+                "name": "Этап пятый",
+                "thresholdPoints": 500,
+                "games": [
+                    {
+                        "name": "Игра 5.1",
+                        "bestResult": 0,
+                        "isPlayed": false
+                    }
+                ]
+            },
+            {
+                "id": 6,
+                "name": "Этап шестой",
+                "thresholdPoints": 1000,
+                "games": [
+                    {
+                        "name": "Игра 6.1",
+                        "bestResult": 100,
+                        "isPlayed": false
+                    },
+                    {
+                        "name": "Игра 6.2",
+                        "bestResult": 0,
+                        "isPlayed": false
+                    }
+                ]
+            }
         ]
     },
     mutations: {
